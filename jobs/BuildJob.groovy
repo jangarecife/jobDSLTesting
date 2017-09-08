02bb78b14c8a98185b92c1150a58bcdc815efa1e
Mads Nielsen <man@praqma.net>
2017-09-08 15:40:35 +0200

    Small change

--- jobs/BuildJob.groovy
+++ jobs/BuildJob.groovy
@@ -5,10 +5,16 @@ def buildJobs = JobSpecification.fromConfig(this.getClass().getResource("product
 buildJobs.each { buildJob ->
     job("${buildJob.jobName}") {
         description("This is the description for the $it.name job")
+        scm {
+            git(buildJob.url)
+        }
         wrappers {
             environmentVariables {
                 env("MSB_OPTIONS", buildJob.msBuildOptions.collect { "/property:${it.key}=${it.value}" }.join(" "))
             }
+        }
+        steps {
+            batchFile("build.bat")
         }
     }
 }
--- src/main/resources/productionJobsList.groovy
+++ src/main/resources/productionJobsList.groovy
@@ -2,10 +2,12 @@ BuildSrc {
     solutionPath = "MyProduct.sln"
     buildMode = "Debug"
     msBuildOptions = [Configuration: "x86"]
+    url = "git@github.com:MyOrg/MyProduct.git"
 }
 
 BuildTest {
     solutionPath = "MyProductTest.sln"
     buildMode = "Debug"
     msBuildOptions = [Configuration: "x86"]
+    url = "git@github.com:MyOrg/MyProduct.git"
 }
